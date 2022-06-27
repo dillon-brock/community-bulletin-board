3707b2aed8c82a655594c6ace8e562ee7e725c2d
Dillon Brock <mdillon1297@gmail.com>
2022-06-27 15:47:26 -0700

    log in and create buttons working on board page if no user, need to build out auth to test other cases

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 // import services and utilities
-import { getBulletins } from './services/bulletin-service.js';
+import { getBulletins, getUser, signOut } from './services/bulletin-service.js';
 // import component creators
 import createBulletinBoard from './components/BulletinBoard.js';
+import createButtons from './components/Buttons.js';
 // declare state variables
 let bulletins = [];
+let user = null;
 // write handler functions
 async function handlePageLoad() {
+    user = await getUser();
     bulletins = await getBulletins();
     display();
+}
+
+function handleAuthRedirect() {
+    user && signOut();
+    window.location.assign('./auth');
+}
+
+function handleCreateRedirect() {
+    const redirectURL = user ? './new' : './auth';
+    window.location.assign(redirectURL);
 }
 // Create each component: 
 // - pass in the root element via querySelector
 // - pass any needed handler functions as properties of an actions object 
 const BulletinBoard = createBulletinBoard(document.getElementById('bulletin-board'));
+const Buttons = createButtons(document.getElementById('buttons'), {
+    handleAuthRedirect,
+    handleCreateRedirect
+});
 // Roll-up display function that renders (calls with state) each component
 function display() {
     // Call each component passing in props that are the pieces of state this component needs
     BulletinBoard({ bulletins });
+    Buttons({ user });
 }
 
 // Call display on page load
