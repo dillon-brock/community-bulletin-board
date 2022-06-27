a7f24cd3f7ffa064d8a0008abf99a6a0380ffdc7
Dillon Brock <mdillon1297@gmail.com>
2022-06-27 14:49:33 -0700

    set up services with core auth functions

---
+++ services/bulletin-service.js
@@ -0,0 +1,21 @@
+const SUPABASE_URL = 'https://nwxkvnsiwauieanvbiri.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAwMzQzNCwiZXhwIjoxOTUyNTc5NDM0fQ.8XIsU0FANdaNeQnT-DojpTL-GTlTPZ4CYZDEetpFpWc';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getUser() {
+    return client.auth.session() && client.auth.session().user;
+}
+
+export async function signUp(email, password) {
+    return client.auth.signUp({ email, password });
+}
+
+export async function signIn(email, password) {
+    return client.auth.signIn({ email, password });
+}
+
+export async function signOut() {
+    return client.auth.signOut();
+}
